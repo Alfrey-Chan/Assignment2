f107877c20b844f94d2955bbb2851aa071455004
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2024-03-23 13:48:38 -0700

    Add controller for handling Transaction-related routes

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
+use Symfony\Component\Console\Output\ConsoleOutput; // for debugging, outputs to terminal 
 
 class TransactionController extends Controller
 {
@@ -13,14 +14,15 @@ class TransactionController extends Controller
      */
     public function index()
     {   
-        // $transactions = Transaction::query()->paginate(5);
-        $transactions = Transaction::all();
+        $transactions = Transaction::orderBy('date', 'desc')
+                                    ->orderBy('id', 'desc')
+                                    ->paginate(10);
         $headers = Schema::getColumnListing('transactions');
         
         return view('transaction.index', [
             'transactions' => $transactions,
             'headers' => $headers
-            ]) ;
+            ])->with(request()->input('page'));
     }
 
     /**
@@ -35,8 +37,32 @@ class TransactionController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        return 'store';
+    {   
+        // validate function will redirect the user to previous location if any checks fail
+        $validatedData = $request->validate([ 
+            'date' => 'required|date',
+            'vendor' => 'required|string',
+            'spend' => 'nullable|numeric',
+            'deposit' => 'nullable|numeric',
+            'balance' => 'required|numeric'
+        ]);
+
+        // $output = new ConsoleOutput();
+        // $output->writeln('validatedData: ' . print_r($validatedData, true));
+
+        try {
+            // check for any negative balances before creating new transaction
+            if (!Transaction::checkNegativeBalances($validatedData)) {
+                return redirect()->back()->withErrors('Transaction failed. Balance cannot be less than 0.');
+            }
+
+            $transaction = Transaction::createNewTransaction($validatedData);
+            Transaction::updateSubsequentBalances($transaction);
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('transaction.index')->with('success', 'Transaction added successfully.');
     }
 
     /**
