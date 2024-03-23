d2a1218cc7eeb16644202cadb11ddc88f57713eb
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2024-03-23 13:46:09 -0700

    Create view for creating new transactions with dynamic error display

--- resources/views/transaction/create.blade.php
+++ resources/views/transaction/create.blade.php
@@ -1,3 +1,39 @@
 <x-layout>
     <h1>create</h1>
+
+    <form action={{ route("transaction.store") }} method="POST">
+        @csrf
+
+        @if ($errors->any())
+            <div class="error-messages">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <label for="date">Date:</label>
+        <input type="date" id="date" name="date">
+
+        <label for="vendor">Vendor:</label>
+        <input type="text" id="vendor" name="vendor">
+        
+        <label for="spend">Spend:</label>
+        <input type="text" id="spend" name="spend">
+
+        <label for="deposit">Deposit</label>
+        <input type="text" id="deposit" name="deposit">
+
+        <label for="balance">Balance</label>
+        <input type="text" id="balance" name="balance">
+
+        <input type="submit" value="Submit">
+    </form>
+
+    @push('scripts')
+        @vite('resources/js/create.js')
+    @endpush
+
 </x-layout>
