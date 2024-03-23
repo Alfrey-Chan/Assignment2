b711dea145cb21f4d07a5815d351566535331452
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2024-03-23 13:47:24 -0700

    Implemented JavaScript to toggle readOnly state between spend and deposit inputs

---
+++ resources/js/create.js
@@ -0,0 +1,22 @@
+const spendInput = document.getElementById("spend");
+const depositInput = document.getElementById("deposit");
+
+spendInput.addEventListener("input", function () {
+    if (this.value.length > 0) {
+        depositInput.readOnly = true;
+        depositInput.value = 0;
+    } else {
+        depositInput.readOnly = false;
+        depositInput.value = "";
+    }
+});
+
+depositInput.addEventListener("input", function () {
+    if (this.value.length > 0) {
+        spendInput.readOnly = true;
+        spendInput.value = 0;
+    } else {
+        spendInput.readOnly = false;
+        spendInput.value = "";
+    }
+});
