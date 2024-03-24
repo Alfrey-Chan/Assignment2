f92dc52dca9b97f6d34cd99ea721ea8cb0e96e4c
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2024-03-24 15:51:04 -0700

    Add 2 routes for get and post requests for importing files

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,9 @@ use App\Http\Controllers\WelcomeController;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
 
+Route::get('transaction/import', [TransactionController::class, 'showImportForm'])->name('transaction.import');
+Route::post('transaction/import', [TransactionController::class, 'importFromCsv'])->name('transaction.import.csv');
+
 // resource method generates several common routes used for Restful controllers
 Route::resource('transaction', TransactionController::class);
+
