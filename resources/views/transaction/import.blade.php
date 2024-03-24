07ee51cc42d4af567c4b02bb8628f6e1b5cf97bc
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2024-03-24 15:59:04 -0700

    Display errors when uploading incorrect file types

--- resources/views/transaction/import.blade.php
+++ resources/views/transaction/import.blade.php
@@ -3,6 +3,16 @@
     <form action="{{ route("transaction.import.csv") }}" method="POST" enctype="multipart/form-data">
         @csrf
 
+        @if ($errors->any())
+            <div class="error-messages text-red-400">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <div class="form-field">
             <label for="fileInput">Upload a CSV file to start organizing your finances!</label>
             <input type="file" name="csvFile" id="fileInput">
